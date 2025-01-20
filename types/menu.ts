f378a88cd0d29081648ce56cdc7a491ce6ff0131
iamabp1 <67823349+iamabp1@users.noreply.github.com>
2025-01-21 00:25:13 +0330

    ffd

--- types/menu.ts
+++ types/menu.ts
@@ -1,7 +1,12 @@
+import { ReactNode } from "react";
+
 export type Menu = {
   id: number;
   title: string;
   path?: string;
+  description?: string;
   newTab: boolean;
-  submenu?: Menu[];
+  megamenu?: boolean; // Optional, only for items with megamenu
+  submenu?: Menu[];   // Nested structure for dropdown or megamenu
+  icon?: any;    // Optional icon for the menu item
 };
