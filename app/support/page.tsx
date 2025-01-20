151bd7d9e301a9d8ed966c464d7b8732b2b677fe
iamabp1 <67823349+iamabp1@users.noreply.github.com>
2025-01-20 23:51:27 +0330

    new

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -3,10 +3,29 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Support Page - Solid SaaS Boilerplate",
-  description: "This is Support page for Solid Pro",
-  // other metadata
-};
+  title: "Support Page - Codeum Smart Contract Audit",
+  description: "Codeum Smart Contract Audit",
+  openGraph: {
+    title: "Codeum | Smart Contract Audit",
+    description: "Protect your projects with Codeum's top-tier smart contract audits, KYC services, and custom blockchain solutions.",
+    images: [
+      {
+        url: "https://codeum.org/images/codeum-ogg.png",
+        alt: "Codeum Smart Contract Audit",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    url: "https://codeum.org",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Codeum | Smart Contract Audit",
+    description: "Protect your projects with Codeum's top-tier smart contract audits, KYC services, and custom blockchain solutions.",
+    images: "https://codeum.org/images/codeum-ogg.png",
+  },
+  };
 
 const SupportPage = () => {
   return (
